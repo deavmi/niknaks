26090fd5f70ad3e5aa58f43c8c17cb40209aa9d3
Tristan B. Velloza Kildaire <deavmi@redxen.eu>
2023-10-30 16:30:38 +0200

    niknaks.debugging
    - Added `isWriteStrat(alias T)(T)`

--- source/niknaks/debugging.d
+++ source/niknaks/debugging.d
@@ -8,6 +8,27 @@ import std.traits : ForeachType, isArray;
 import std.conv : to;
 import std.stdio : writeln;
 
+import std.traits : isFunction, arity, ParameterTypeTuple;
+
+private bool isWriteStrat(alias T)(T)
+{
+    static if(isFunction!(T))
+    {
+        alias paramTypes = ParameterTypeTuple!(T);
+        static if(arity!(T) == 1 && __traits(isSame, paramTypes[0], string))
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+    else
+    {
+        return false;
+    }
+}
 
 public void dumpArray(T)(T[] array, size_t start, size_t end)
 {
