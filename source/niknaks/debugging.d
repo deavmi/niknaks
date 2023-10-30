9d8a0bd4096169df129b96569fa6fbf7373ed183
Tristan B. Velloza Kildaire <deavmi@redxen.eu>
2023-10-30 20:07:52 +0200

    niknaks.debugging
    - Removed dead and old code
    - Cleaned up unused imports

--- source/niknaks/debugging.d
+++ source/niknaks/debugging.d
@@ -4,34 +4,8 @@
  */
 module niknaks.debugging;
 
-import std.traits : ForeachType, isArray;
+import std.traits : isArray;
 import std.conv : to;
-
-import std.traits : isFunction, arity, ParameterTypeTuple;
-
-private bool isWriteStrat(alias T)(T)
-{
-    static if(isFunction!(T))
-    {
-        alias paramTypes = ParameterTypeTuple!(T);
-        static if(arity!(T) == 1 && __traits(isSame, paramTypes[0], string))
-        {
-            return true;
-        }
-        else
-        {
-            return false;
-        }
-    }
-    else
-    {
-        return false;
-    }
-}
-
-// public alias Strat = void function(string...);
-
-// public Strat defaultStrat = &writeln;
 
 /** 
  * Generates a string containing
