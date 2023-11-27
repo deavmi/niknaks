89155e385952f5c17eda030e705e276024161edd
Tristan B. Velloza Kildaire <deavmi@redxen.eu>
2023-11-27 17:51:31 +0200

    Entry
    - Added entry

--- source/niknaks/containers.d
+++ source/niknaks/containers.d
@@ -31,3 +31,42 @@ public enum ExpirationStrategy
     ON_ACCESS // TODO: Tweak how ofteh this is to make it low O(n) because currently it is 1*O(n) which is bruh moment for a hashmap my fella
 }
 
+public template Entry(V)
+{
+    public struct Entry
+    {
+        private V value;
+        private StopWatch timer;
+
+        @disable
+        private this();
+
+        public this(V value)
+        {
+            setValue(value);
+            timer = StopWatch(AutoStart.yes);
+        }
+
+        public void setValue(V value)
+        {
+            this.value = value;
+        }
+
+        public V getValue()
+        {
+            return this.value;
+        }
+
+        public void bump()
+        {
+            timer.reset();
+        }
+
+        public Duration getElapsedTime()
+        {
+            return timer.peek();
+        }
+    }
+}
+
+
