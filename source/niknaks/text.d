2ee03bd5bd4ef3a6f2c3ed1f7d55c9156698e0d0
Tristan B. Velloza Kildaire <deavmi@redxen.eu>
2024-08-19 18:49:22 +0200

    niknaks.text
    - Added new module
    - Implemented `padLeft(string, string)`

---
+++ source/niknaks/text.d
@@ -0,0 +1,49 @@
+/**
+ * Text manioulation
+ *
+ * Authors: Tristan Brice Velloza Kildaire
+ */
+module niknaks.text;
+
+import std.string : join, format, split;
+
+/** 
+ * Pads the left-hand margin of a multi-line
+ * string with the given text
+ *
+ * Params:
+ *   bodyText = the text to pad
+ *   withText = the padding text
+ * Returns: the padded text
+ */
+public string padLeft(string bodyText, string withText)
+{
+    string[] lines_out;
+    string[] lines_in = split(bodyText, "\n");
+    foreach(string l; lines_in)
+    {
+        lines_out ~= format("%s%s", withText, l);
+    }
+
+    return join(lines_out, "\n");
+}
+
+version(unittest)
+{
+    import std.stdio;
+}
+
+unittest
+{
+    string input = `Hello
+World
+`;
+
+    string output = padLeft(input, "%");
+
+    string[] output_segments = output.split("\n");
+    writeln(output_segments);
+    assert(output_segments[0] == "%Hello");
+    assert(output_segments[1] == "%World");
+    assert(output_segments[2] == "%");
+}
